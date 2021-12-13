139e8ab35006adeb3a8e035d58f7b163e299b47b
Oscar Santiago <osantiag@gmail.com>
2021-12-13 18:39:28 +0100

    finished tests

--- app/routers/post.py
+++ app/routers/post.py
@@ -45,7 +45,7 @@ def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
 def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     # takes post and converts it to a dictionary and unpack it with **
-    print(current_user.email)
+    #print(current_user.email)
     new_post = models.Post(owner_id=current_user.id, **post.dict())
     db.add(new_post)
     db.commit()
@@ -81,13 +81,23 @@ def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), curren
 def get_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     # post = db.query(models.Post).filter(models.Post.id == id).first()
     post = db.query(models.Post, func.count(models.Vote.post_id).label("votes")).join(models.Vote, models.Vote.post_id == models.Post.id,
-                                                                                      isouter=True).group_by(models.Post.id).first()
+                                                                                      isouter=True).group_by(models.Post.id).filter(models.Post.id == id).first()
+    print(post)
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id: {id} was not found")
         # response.status_code = status.HTTP_404_NOT_FOUND
         # return {'message': f"post with id: {id} was not found"}
     return post
+
+    # post = db.query(models.Post, func.count(models.Vote.post_id).label("votes")).join(
+    #     models.Vote, models.Vote.post_id == models.Post.id, isouter=True).group_by(models.Post.id).filter(models.Post.id == id).first()
+
+    # if not post:
+    #     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+    #                         detail=f"post with id: {id} was not found")
+
+    # return post
 
 # just for testing purposes as first step, when working without databases
 # @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
--- test/test_users.py
+++ test/test_users.py
@@ -1,11 +1,14 @@
 from app import schemas
-from .database import client,session
+import pytest
+from jose import jwt
+from app.config import settings
 
-def test_root(client):
-    res = client.get("/")
-    print(res.json().get('message'))
-    assert res.json().get('message') == 'Welcome to my super visual studio API!!!Hurrafadfad'
-    assert res.status_code == 200
+# def test_root(client):
+#     res = client.get("/")
+#     print(res.json().get('message'))
+#     assert res.json().get('message') == 'Welcome to my super visual studio API!!!Hurrafadfad'
+#     assert res.status_code == 200
+
 
 def test_create_user(client):
     res = client.post("/users/",json={"email":"hihi@gmail.com", "password":"abc123"})
@@ -14,3 +17,27 @@ def test_create_user(client):
     assert new_user.email == "hihi@gmail.com"
     # assert res.json().get("email") == "halli@gmail.com"
     # assert res.status_code == 201
+
+def test_login_user(client, test_user):
+    res = client.post("/login",data={"username":test_user['email'], "password":test_user['password']})
+    # print(res.json())
+    login_res = schemas.Token(**res.json())
+    payload = jwt.decode(login_res.access_token, settings.secret_key, settings.algorithm)
+    id = payload.get("user_id")
+    assert id == test_user['id']
+    assert login_res.token_type == "bearer"
+    assert res.status_code == 200
+
+@pytest.mark.parametrize("email, password, status_code",[
+    ('wrongemail@gmail.com','abc123', 403),
+    ('hihi@gmail.com','wrongPassword', 403),
+    ('wrongemail@gmail.com','wrongPassword', 403),
+    (None,'abc123', 422),
+    ('hihi@gmail.com','abc1234', 403),
+    ('hihi@gmail.com',None, 422),
+])
+def test_incorrect_login(test_user, client, email, password, status_code):
+    res = client.post("/login", data={"username": email, "password": password})
+
+    assert res.status_code == status_code
+    # assert res.json().get('detail') == 'Invalid Credentials'
